4472a68d3e6250fd841f60bd562864f231723e43
Emulie Chhor <emulie.chhor@hotmail.com>
2021-01-23 14:26:09 -0500

    updated reverse solution for #189: Rotate Array

--- 189-RotateArray-reverse.java
+++ 189-RotateArray-reverse.java
@@ -1,21 +1,22 @@
 #!/usr/bin/java
 
 class Solution {
-    public void rotate(int[] nums, int k) {        
-        // Using Reverse
-        // 1. Reverse array
-        // 2. Reverse first k element
-        // 3. Reverse n-k element
+    public void rotate(int[] nums, int k) {
+        // Reverse Solution
+		// 1. Reverse whole array
+		// 2. Reverse from first to k element
+		// 3. Reverse from k to last element
+        k = k % nums.length;
         reverse(nums, 0, nums.length -1);
-        reverse(nums, 0, k -1);
+        reverse(nums, 0, k-1);
         reverse(nums, k, nums.length -1);
     }
     
-    private void reverse(int[] nums, int left, int right){
-        while(left < right){
-            int temp = nums[left];
-            nums[left++] = nums[right];
-            nums[right--] = temp;
+    private void reverse(int[] nums, int start, int end){
+        while (start < end){
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
         }
     }
 }
