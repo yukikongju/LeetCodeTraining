891cda41e02ba3157a1520b12e6b017393afba96
Emulie Chhor <emulie.chhor@hotmail.com>
2021-01-23 12:09:28 -0500

    added interface for ADT: Stack

---
+++ ADT/Stack.java
@@ -0,0 +1,32 @@
+#!/usr/bin/java
+
+public Stack implements Iterable{
+  private Object[] elements;
+  private int top;
+  private static final DEFAULT_CAPACITY = 1;
+
+  // init
+  private Bag(){this(DEFAULT_CAPACITY);}
+  private Bag(int capacity){elements = new Object[capacity]; top = 0;}
+
+  // push
+  private void push(Object object){
+	if(top == elements.length) realloc(2*elements.length);
+	elements[top++] = object;
+  }
+
+  private void realloc(int newSize){
+	Object[] temp = new Object[newSize];
+	for(int i=0; i<elements.length; i++) temp[i] = elements[i];
+	elements = temp;
+  }
+
+  // pop
+  private void pop(){
+	Object lastElement = elements[top];
+	top--;
+	elements[top] = null;
+	if(top < elements.length / 4) realloc(elements.length /2);
+	return lastElement;
+  }
+}
