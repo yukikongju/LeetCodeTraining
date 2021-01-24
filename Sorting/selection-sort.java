798dc4bf5aa82602d23465dac528fc59deebcc0c
Emulie Chhor <emulie.chhor@hotmail.com>
2021-01-24 16:42:45 -0500

    implemented selection sort with Comparable

---
+++ Sorting/selection-sort.java
@@ -0,0 +1,26 @@
+#!/usr/bin/java
+
+public class Selection {
+  public static void selectionSort(int[] array){
+	for(int i=0; i< array.length - 1; i++){
+	  int min = i;
+	  for(int j = i+1; j< array.length; j++){
+		if(less(array[j], array[min])){
+		  min = j;
+		}
+		exch(array, i, min);
+	  }
+	}
+  }
+
+  private static void less(Comparable item1, Comparable item2){
+	return item1.compareTo(item2) < 0;
+  }
+
+  private static void exch(Comparable[] array, int i, int j){
+	// swap position
+	int temp = array[i];
+	array[i] = array[min];
+	array[min] = temp;
+  }
+}
