3a8cc6fe8a16ddcf5fb281874105fd5ee0f31f55
Emulie Chhor <emulie.chhor@hotmail.com>
2021-01-28 19:27:04 -0500

    added recursive solution to #70: Climing Stairs

---
+++ 70-ClimbingStairs-recursive.java
@@ -0,0 +1,10 @@
+#!/usr/bin/java
+
+class Solution {
+    public int climbStairs(int n) {
+        // Recursive Solution
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+}
