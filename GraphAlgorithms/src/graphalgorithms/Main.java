6898540590ca4db4f6f106588f80e3c83ebb712b
Emulie Chhor <emulie.chhor@hotmail.com>
2021-07-14 11:59:58 -0400

    added dummy graph for testing

--- GraphAlgorithms/src/graphalgorithms/Main.java
+++ GraphAlgorithms/src/graphalgorithms/Main.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package graphalgorithms;
 
-/**
- *
- * @author emuli
- */
-public class GraphAlgorithms {
+public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static final String START = "a";
+    private static final String END = "e";
+
+    private static final Graph.Edge[] GRAPH = {
+        new Graph.Edge("a", "b", 7),
+        new Graph.Edge("a", "c", 9),
+        new Graph.Edge("a", "f", 14),
+        new Graph.Edge("b", "c", 10),
+        new Graph.Edge("b", "d", 15),
+        new Graph.Edge("c", "d", 11),
+        new Graph.Edge("c", "f", 2),
+        new Graph.Edge("d", "e", 6),
+        new Graph.Edge("e", "f", 9),};
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
-    
+
 }
