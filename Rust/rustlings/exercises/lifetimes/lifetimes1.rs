50b5018c9336537eac36981bef06f01644b80253
Emulie Chhor <emulie.chhor@hotmail.com>
2022-07-29 10:00:18 -0400

    added rustlings solutions for quiz, std libs iterators and move_semantics

--- Rust/rustlings/exercises/lifetimes/lifetimes1.rs
+++ Rust/rustlings/exercises/lifetimes/lifetimes1.rs
@@ -7,7 +7,7 @@
 //
 // Execute `rustlings hint lifetimes1` or use the `hint` watch subcommand for a hint.
 
-fn longest<'a, 'b>(x: &'a str, y: &'b str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
