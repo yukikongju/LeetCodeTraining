5727761d0ae79b82dbf83fdf8d4afc219845b998
Emulie Chhor <emulie.chhor@hotmail.com>
2023-11-05 14:40:04 -0500

    added sol to aoc2021 day06b

--- AdventOfCode/2021/day06/src/main.rs
+++ AdventOfCode/2021/day06/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     // -- 1. 
-    let lanterns: Vec<i32> = include_str!("../inputs/2.txt")
+    let lanterns: Vec<usize> = include_str!("../inputs/2.txt")
         .lines()
         .flat_map(|l| l.split(",").map(|c| c.parse().unwrap()))
         .collect();
@@ -18,7 +18,18 @@ fn main() {
         // println!("{:?}", population);
     }
 
-    print!("[ Part 1 ] Population size: {}", population.len());
+    println!("[ Part 1 ] Population size: {}", population.len());
 
+    // [ PART 2 - Using Map and array rotation]
+    let mut map = lanterns.clone()
+        .iter()
+        .fold([0; 9], |mut map, &n| {
+            map[n] += 1;
+            map
+        });
+
+    (1..256).for_each(|day| map[(day + 7) % 9] += map[day % 9]);
+    let pop_size: usize = map.iter().sum::<usize>();
+    println!("[ Part 2 ] Population size: {}", pop_size);
 
 }
