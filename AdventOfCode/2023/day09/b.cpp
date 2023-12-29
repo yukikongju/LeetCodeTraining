132e0813282d0a08cbd395b3c56a9828245d882e
Emulie Chhor <emulie.chhor@hotmail.com>
2023-12-29 13:14:19 -0500

    added sol to aoc2023 day09b

---
+++ AdventOfCode/2023/day09/b.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#include <numeric>
+using namespace std;
+
+int main() {
+  // 1. read inputs
+  string FILENAME = "inputs/2.txt";
+  ifstream file(FILENAME);
+  string line;
+  vector<vector<int>> histories;
+
+  while (getline(file, line)) {
+    vector<int> history;
+    istringstream ss(line);
+    int number;
+    while (ss >> number)
+      history.push_back(number);
+    histories.push_back(history);
+  }
+
+  // 2. [ PART 2 - Compute sum of extrapolated values for start of sequences ]
+  vector<long long> extrapolatedValues;
+  for (auto &history : histories) {
+    // compute differences
+    vector<vector<int>> differences;
+    differences.push_back(history);
+
+    bool isDifferenceZero = false;
+    while (!isDifferenceZero) {
+      // compute differences
+      vector<int> difference;
+      auto lastVector = differences.back();
+      for (int i = 0; i < lastVector.size() - 1; i++) {
+        difference.push_back(lastVector[i + 1] - lastVector[i]);
+      }
+      differences.push_back(difference);
+
+      // check if all difference is null to exit loop
+      bool isZero = true;
+      for (auto &n : difference)
+        if (n != 0)
+          isZero = false;
+      isDifferenceZero = isZero;
+    }
+
+    // print
+    // for (auto &difference : differences) {
+    //   for (auto &d : difference)
+    //     cout << d << ' ';
+    //   cout << '\n';
+    // }
+
+    // get extrapolated value from differences
+    long long value =
+        accumulate(differences.rbegin(), differences.rend(), 0,
+                   [](int partialSum, const std::vector<int> &innerVec) {
+                     return innerVec.front() - partialSum;
+                   });
+    extrapolatedValues.push_back(value);
+  }
+
+  // print
+  // for (auto &n : extrapolatedValues)
+  //   cout << n << ' ';
+
+  long long sum =
+      accumulate(extrapolatedValues.begin(), extrapolatedValues.end(), 0);
+  cout << "[ PART 2 ] Extrapolated Value Sum: " << sum << '\n';
+
+  return 0;
+}
