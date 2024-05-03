e3a80a0efbbf56cde0a1f9b241c84ed285c39f92
yukikongju <emulie.chhor@hotmail.com>
2024-05-03 01:35:46 -0400

    implemented operator function to use sorting algorithm

--- AdventOfCode/2023/day07/a.cpp
+++ AdventOfCode/2023/day07/a.cpp
@@ -48,37 +48,67 @@ struct Hand {
     }
     cout << "\n";
   }
-};
 
-bool compareHandByCards(Hand &hand1, Hand &hand2) { // TODO
-  for (int i = 5; i >= 1; i--) {
-    auto it1 = hand1.cardsValuesDict.find(i);
-    auto it2 = hand2.cardsValuesDict.find(i);
-    if ((it1 != hand1.cardsValuesDict.end()) &&
-        (it2 == hand2.cardsValuesDict.end())) {
-      return true;
-    } else if ((it1 != hand1.cardsValuesDict.end()) &&
-               (it2 == hand2.cardsValuesDict.end())) {
-      return false;
-    } else {
-      for (char c : cardsOrderingDesc) {
-        auto f1 = find(hand1.cardsValuesDict[i].begin(),
-                       hand1.cardsValuesDict[i].end(), c);
-        auto f2 = find(hand2.cardsValuesDict[i].begin(),
-                       hand2.cardsValuesDict[i].end(), c);
-        if ((f1 != hand1.cardsValuesDict[i].end()) &&
-            (f2 == hand2.cardsValuesDict[i].end())) {
-          return true;
-        } else if ((f1 == hand1.cardsValuesDict[i].end()) &&
-                   (f2 != hand2.cardsValuesDict[i].end())) {
-          return false;
+  bool operator<(const Hand &other) const {
+    for (int i = 5; i >= 1; i--) {
+      auto it1 = cardsValuesDict.find(i);
+      auto it2 = other.cardsValuesDict.find(i);
+
+      // If the current hand has a higher card count, it is greater
+      if (it1 != cardsValuesDict.end() && it2 == other.cardsValuesDict.end()) {
+        return false;
+      } else if (it1 == cardsValuesDict.end() &&
+                 it2 != other.cardsValuesDict.end()) {
+        return true;
+      } else if (it1 != cardsValuesDict.end() &&
+                 it2 != other.cardsValuesDict.end()) {
+        // Compare the card values in descending order
+        for (char c : cardsOrderingDesc) {
+          auto f1 = find(it1->second.begin(), it1->second.end(), c);
+          auto f2 = find(it2->second.begin(), it2->second.end(), c);
+          if (f1 != it1->second.end() && f2 == it2->second.end()) {
+            return false;
+          } else if (f1 == it1->second.end() && f2 != it2->second.end()) {
+            return true;
+          }
         }
       }
     }
-  }
 
-  return true;
-}
+    // If all the above comparisons are equal, the hands are considered equal
+    return false;
+  }
+};
+
+// bool compareHandByCards(Hand &hand1, Hand &hand2) { // TODO
+//   for (int i = 5; i >= 1; i--) {
+//     auto it1 = hand1.cardsvaluesdict.find(i);
+//     auto it2 = hand2.cardsvaluesdict.find(i);
+//     if ((it1 != hand1.cardsvaluesdict.end()) &&
+//         (it2 == hand2.cardsvaluesdict.end())) {
+//       return true;
+//     } else if ((it1 != hand1.cardsvaluesdict.end()) &&
+//                (it2 == hand2.cardsvaluesdict.end())) {
+//       return false;
+//     } else {
+//       for (char c : cardsorderingdesc) {
+//         auto f1 = find(hand1.cardsvaluesdict[i].begin(),
+//                        hand1.cardsvaluesdict[i].end(), c);
+//         auto f2 = find(hand2.cardsvaluesdict[i].begin(),
+//                        hand2.cardsvaluesdict[i].end(), c);
+//         if ((f1 != hand1.cardsvaluesdict[i].end()) &&
+//             (f2 == hand2.cardsvaluesdict[i].end())) {
+//           return true;
+//         } else if ((f1 == hand1.cardsvaluesdict[i].end()) &&
+//                    (f2 != hand2.cardsvaluesdict[i].end())) {
+//           return false;
+//         }
+//       }
+//     }
+//   }
+
+//   return true;
+// }
 
 int main() {
   // 1. read inputs
@@ -104,6 +134,11 @@ int main() {
   }
 
   // 2. Implement sorting algorithm to rank hands
+  sort(hands.begin(), hands.end());
+  cout << "After sorting \n";
+  for (auto &hand : hands) {
+    hand.print();
+  }
 
   return 0;
 }
