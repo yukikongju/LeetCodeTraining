8730835d0a9320ff0ea26b8db99d7776d475de02
yukikongju <emulie.chhor@hotmail.com>
2024-05-03 23:16:39 -0400

    added function to check if candidate is valid

--- AdventOfCode/2023/day12/a.cpp
+++ AdventOfCode/2023/day12/a.cpp
@@ -40,6 +40,60 @@ Spring parse_spring(const string &line) {
   return spring;
 }
 
+bool isValidCandidate(string &candidate, vector<int> &grouping) {
+  // 1. calculate groupings of '#' in candidate
+  vector<int> candidateGrouping;
+  int groupingLength = 0;
+  for (char c : candidate) {
+    if (c == '#') {
+      groupingLength++;
+    } else if (c == '.' && groupingLength > 0) {
+      candidateGrouping.push_back(groupingLength);
+      groupingLength = 0;
+    }
+  }
+  if (groupingLength > 0) {
+    candidateGrouping.push_back(groupingLength);
+  }
+
+  // 2. compare candidate grouping vs arrangements
+  if (candidateGrouping.size() != grouping.size()) {
+    return false;
+  }
+
+  // compare value 1 by 1
+  int n = candidateGrouping.size();
+  for (int i = 0; i < n; i++) {
+    if (candidateGrouping[i] != grouping[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+vector<string> generateCandidates(string &arrangement) { // TODO
+  vector<string> candidates;
+  return candidates;
+}
+
+int getNumArrangement(Spring &spring) {
+  // Brute-Force solution: O(2^n), where n is the number of '?'
+  // (1) generate a candidate by replacing '?' by # or .
+  // (2) verify if candidate is valid
+
+  int numArrangements = 0;
+
+  vector<string> candidates = generateCandidates(spring.arrangement);
+  for (string candidate : candidates) {
+    if (isValidCandidate(candidate, spring.groupings)) {
+      numArrangements++;
+    }
+  }
+
+  return numArrangements;
+}
+
 int main() {
   // 1. Read inputs
   string FILENAME = "inputs/1.txt";
@@ -51,11 +105,18 @@ int main() {
     springs.push_back(parse_spring(line));
   }
 
-  for (auto &s : springs) {
-    s.print();
-  }
+  // printing
+  // for (auto &s : springs) {
+  //   s.print();
+  // }
 
   // 2.
+  long long sumArrangements = 0;
+  for (auto &spring : springs) {
+    sumArrangements += getNumArrangement(spring);
+  }
+
+  cout << "Sum Arrangements: " << sumArrangements << "\n";
 
   return 0;
 }
