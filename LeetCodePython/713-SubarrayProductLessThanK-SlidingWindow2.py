53118cabca423f3362bc7ef003bb7f188c9bb4c3
yukikongju <emulie.chhor@hotmail.com>
2024-09-21 12:47:52 -0400

    added solution to 713

---
+++ LeetCodePython/713-SubarrayProductLessThanK-SlidingWindow2.py
@@ -0,0 +1,19 @@
+#  https://leetcode.com/problems/subarray-product-less-than-k/description/
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        # solution: two pointers // sliding window O(n)
+        if k <= 1:
+            return 0
+
+        count = 0
+        left = 0
+        product = 1
+        
+        for right in range(len(nums)):
+            product *= nums[right]
+            while product >= k:
+                product /= nums[left]
+                left += 1
+            count += right - left + 1
+        return count 
+
