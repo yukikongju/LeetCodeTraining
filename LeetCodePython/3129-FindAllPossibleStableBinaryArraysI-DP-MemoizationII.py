38cbee2c8cff6ce6500fa0d3dbcf4d6fc790843b
yukikongju <emulie.chhor@hotmail.com>
2024-11-24 11:34:40 -0500

    added sol to 3129

---
+++ LeetCodePython/3129-FindAllPossibleStableBinaryArraysI-DP-MemoizationII.py
@@ -0,0 +1,28 @@
+#  https://leetcode.com/problems/find-all-possible-stable-binary-arrays-i/
+
+class Solution:
+    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+        
+        memo = {}
+        MOD = 10**9 + 7
+
+        def dp(z, o, p):
+            if z == 0 and o == 0:
+                return 1
+            
+            if (z, o, p) in memo:
+                return memo[(z, o, p)]
+            
+            answer = 0
+            for i in range(1, limit+1):
+                if (p == 0 or p == -1) and (o-i>=0):
+                    answer += dp(z, o-i, 1)
+                if (p == 1 or p == -1) and (z-i>=0):
+                    answer += dp(z-i, o, 0)
+            
+            memo[(z, o, p)] = answer % MOD
+            return memo[(z, o, p)]
+        
+        return dp(zero, one, -1) % MOD
+        
+        
