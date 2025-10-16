a2ce92fcbc19d7d8e2da6092cd0e8bc422d6d42d
yukikongju <emulie.chhor@hotmail.com>
2025-10-15 21:03:01 -0400

    initialized some code

---
+++ Clang/CTheHardWay/ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+  // breaking it: set int age;
+  int age = 10;
+  int height = 72;
+
+  printf("I am %d years old\n", age);
+  printf("I am %d inch tall\n", height);
+}
